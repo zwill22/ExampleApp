6aad17a5fc95a7ffd5f44678c402c706bbc040c0
Zack Williams <73105122+zwill22@users.noreply.github.com>
2024-07-25 15:09:09 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Zack Williams on 25/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
