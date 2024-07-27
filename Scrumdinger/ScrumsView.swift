80ec4e13aef448a7a78962771c9e35983e141e9f
Zack Williams <73105122+zwill22@users.noreply.github.com>
2024-07-27 16:06:33 +0100

    Displaying data in a list

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,25 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Zack Williams on 27/07/2024.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColour)
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
