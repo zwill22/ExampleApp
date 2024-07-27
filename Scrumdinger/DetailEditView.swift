a1b1866f5370731f6356ddc28c81da18b12ee47d
Zack Williams <73105122+zwill22@users.noreply.github.com>
2024-07-27 19:25:02 +0100

    Created an edit view

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,59 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Zack Williams on 27/07/2024.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    @State private var scrum = DailyScrum.emptyScrum
+    @State private var newAttendeeName = ""
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info")){
+                TextField("Title", text: $scrum.title)
+                HStack {
+                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
+                        Text("Length")
+                    }
+                    .accessibilityValue("\(scrum.lengthInMinutes) minutes")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                        .accessibilityHidden(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                }
+            }
+            
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indices in
+                    scrum.attendees.remove(atOffsets: indices)
+                }
+                HStack {
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(
+                        action: {
+                            withAnimation {
+                                let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                                scrum.attendees.append(attendee)
+                                newAttendeeName = ""
+                            }
+                        },
+                        label: {
+                            Image(systemName:  "plus.circle.fill")
+                                .accessibilityLabel("Add attendee")
+                        }
+                    ).disabled(newAttendeeName.isEmpty)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    DetailEditView()
+}
