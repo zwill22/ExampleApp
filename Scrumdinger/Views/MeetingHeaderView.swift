b9e1221da59c8d8c9805b96847fc49593fa1e110
Zack Williams <73105122+zwill22@users.noreply.github.com>
2024-07-28 18:18:52 +0100

    Split MeetingView into header and footer

--- Scrumdinger/Views/MeetingHeaderView.swift
+++ Scrumdinger/Views/MeetingHeaderView.swift
@@ -8,11 +8,49 @@
 import SwiftUI
 
 struct MeetingHeaderView: View {
+    let secondsElapsed: Int
+    let secondsRemaining: Int
+    let theme: Theme
+    
+    private var totalSeconds: Int {
+        secondsElapsed + secondsRemaining
+    }
+    
+    private var progress: Double {
+        guard totalSeconds > 0 else { return 1 }
+        
+        return Double(secondsElapsed) / Double(totalSeconds)
+    }
+    
+    private var minutesRemaining: Int {
+        secondsRemaining / 60
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ProgressView(value: progress)
+                .progressViewStyle(ScrumProgressViewStyle(theme: theme))
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed").font(.caption)
+                    Label("\(secondsElapsed)", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing){
+                    Text("Seconds Remaining").font(.caption)
+                    Label("\(secondsRemaining)", systemImage: "hourglass.bottomhalf.fill")
+                        .labelStyle(.trailingIcon)
+                }
+            }
+        }
+        .accessibilityElement(children: /*@START_MENU_TOKEN@*/.ignore/*@END_MENU_TOKEN@*/)
+        .accessibilityLabel("Time remaining")
+        .accessibilityValue("\(minutesRemaining) minutes")
+        .padding([.top, .bottom])
     }
 }
 
 #Preview {
-    MeetingHeaderView()
+    MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180, theme: .bubblegum)
+        .previewLayout(.sizeThatFits)
 }
--- Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Views/MeetingView.swift
@@ -8,37 +8,38 @@
 import SwiftUI
 
 struct MeetingView: View {
+    @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
+    
     var body: some View {
-        VStack {
-            ProgressView(value: 5, total: 15)
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("Seconds Elapsed").font(.caption)
-                    Label("300", systemImage: "hourglass.tophalf.fill")
-                }
-                Spacer()
-                VStack(alignment: .trailing){
-                    Text("Seconds Remaining").font(.caption)
-                    Label("600", systemImage: "hourglass.bottomhalf.fill")
-                }
+        ZStack{
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.theme.mainColour)
+            VStack {
+                MeetingHeaderView(
+                    secondsElapsed: scrumTimer.secondsElapsed,
+                    secondsRemaining: scrumTimer.secondsRemaining,
+                    theme: scrum.theme)
+                Circle().strokeBorder(lineWidth: 24)
+                MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
-            .accessibilityElement(children: /*@START_MENU_TOKEN@*/.ignore/*@END_MENU_TOKEN@*/)
-            .accessibilityLabel("Time remaining")
-            .accessibilityValue("10 minutes")
-            Circle().strokeBorder(lineWidth: 24)
-            HStack{
-                Text("Speaker 1 of 3")
-                Spacer()
-                Button(action: {}) {
-                    Image(systemName: "forward.fill")
-                }.accessibilityLabel("Next Speaker")
-            }
-        }.padding()
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColour)
+        .onAppear {
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes,
+                             attendees: scrum.attendees)
+            scrumTimer.startScrum()
+        }
+        .onDisappear {
+            scrumTimer.stopScrum()
+        }
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingView()
+        MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
     }
 }
